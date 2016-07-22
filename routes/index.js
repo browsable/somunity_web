d4bfb36246a0d63e84d37cbf850e1be51c68dbf4
browsable <wlsehfdl1>
2016-07-22 10:06:29 +0900

    fcm & node - push

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@
 
     //routing 처리
     var express = require("express"),
+        FCM = require("fcm-node"),
         controller = require("../controller"),
         router = express.Router();
+    var serverKey='AIzaSyCr7G2QlSJbZI8L4oC7GyoC-m7GoPjo2ZM';
+    var fcm = new FCM(serverKey);
 
     router.get("/", function (req, res) {
         res.render("index",{
@@ -59,4 +62,40 @@
         var keyword = req.body.keyword;
         controller.listSearch(req,res,keyword);
     });
-    module.exports = router; //c
+
+    //request from android app
+    router.post('/fcm',function (req, res) {
+        var registration_token = req.body.registration_token,
+            name = req.body.name,
+            tag = req.body.tag;
+        if (tag === "fcm") {
+            if (errors) {
+                res.json({message: errors});
+            } else {
+                var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+                    to: registration_token,
+                    collapse_key: 'invite_msg',
+                    data: {
+                        msg: name+'님으로부터 친구 요청이 왔습니다.'
+                    }
+                    /*notification: {
+                        title: '친구 요청이 왔습니다.',
+                        body: name+'님으로부터 친구 요청이 왔습니다.',
+                        icon: 'ic_launcher' //now required
+                    }*/
+                };
+                fcm.send(message, function(err, response){
+                    if (err) {
+                        console.log("Something has gone wrong!");
+                    } else {
+                        console.log("Successfully sent with response: ", response);
+                        res.json({success: "1", message: "push success!"});
+                    }
+                });
+            }
+        } else {
+            res.json({success: "2", message: "not a fcm request"});
+        }
+        console.log("routing complete");
+    });
+    module.exports = router;
